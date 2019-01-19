c741db13315f896860e262a62eac0c5a0c3c8535
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-01-19 10:49:08 +0100

    Update gamer version

--- src/game/version.h
+++ src/game/version.h
@@ -4,7 +4,7 @@
 #define GAME_VERSION_H
 // #include <generated/nethash.cpp>
 #define GAME_NETVERSION_HASH "802f1be60a05665f"
-#define GAME_VERSION "0.7.2 - Gamer 5.1"
+#define GAME_VERSION "0.7.2 - Gamer 5.2"
 #define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH
 #define CLIENT_VERSION 0x0703
 static const char GAME_RELEASE_VERSION[8] = "0.7.2";
