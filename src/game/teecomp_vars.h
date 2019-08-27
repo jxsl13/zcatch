b4bf4f133305a0f8c4f0487d7e11f72fd23db25f
Jordy Ruiz <me@jruiz.fr>
2019-08-27 18:49:48 +0200

    Change some variables' defaults

--- src/game/teecomp_vars.h
+++ src/game/teecomp_vars.h
@@ -2,10 +2,10 @@
 MACRO_CONFIG_INT(TcColoredTeesMethod, tc_colored_tees_method, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Enable enemy based skin colors")
 MACRO_CONFIG_INT(TcDmColorsTeam1, tc_dm_colors_team1, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Use DM colors for red team/team mates")
 MACRO_CONFIG_INT(TcDmColorsTeam2, tc_dm_colors_team2, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Use DM colors for blue team/enemies")
-MACRO_CONFIG_INT(TcColoredTeesTeam1, tc_colored_tees_team1, 16739179, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Red team/team mates color") // TODO: move this hack to static vars
-MACRO_CONFIG_INT(TcColoredTeesTeam2, tc_colored_tees_team2, 7053311, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Blue team/enemies color")
-MACRO_CONFIG_INT(TcColoredTeesTeam1Hsl, tc_colored_tees_team1_hsl, 16739179, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Red team/team mates color")
-MACRO_CONFIG_INT(TcColoredTeesTeam2Hsl, tc_colored_tees_team2_hsl, 16739179, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Red team/team mates color")
+MACRO_CONFIG_INT(TcColoredTeesTeam1, tc_colored_tees_team1, /* 16739179 */ -1, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Red team/team mates color") // TODO: move this hack to static vars
+MACRO_CONFIG_INT(TcColoredTeesTeam2, tc_colored_tees_team2, /* 7053311 */ -1, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Blue team/enemies color")
+MACRO_CONFIG_INT(TcColoredTeesTeam1Hsl, tc_colored_tees_team1_hsl, -1, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Red team/team mates color")
+MACRO_CONFIG_INT(TcColoredTeesTeam2Hsl, tc_colored_tees_team2_hsl, -1, -1, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Red team/team mates color")
 
 MACRO_CONFIG_INT(TcForcedSkinsMethod, tc_forced_skins_method, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Enable enemy based forced skins")
 MACRO_CONFIG_INT(TcForceSkinTeam1, tc_force_skin_team1, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Force a skin for red team/your team/DM matchs")
@@ -15,7 +15,7 @@ MACRO_CONFIG_STR(TcForcedSkin2, tc_forced_skin2, 64, "default", CFGFLAG_CLIENT|C
 
 MACRO_CONFIG_INT(TcNameplateScore, tc_nameplate_score, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Display score on name plates")
 
-MACRO_CONFIG_INT(TcHudMatch, tc_hud_match, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Make HUD match tees' colors")
+MACRO_CONFIG_INT(TcHudMatch, tc_hud_match, 1, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Make HUD match tees' colors")
 
 MACRO_CONFIG_INT(TcLaserColorInner, tc_laser_color_inner, 8355839, 0, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Inner color of laser")
 MACRO_CONFIG_INT(TcLaserColorOuter, tc_laser_color_outer, 1250112, 0, 16777215, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Outer color of laser")
