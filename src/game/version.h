fb3b8d515b2303011e508a97e53cbfea32711c43
Jordy Ruiz <jordy.ruiz@univ-lille.fr>
2019-09-08 12:11:56 +0200

    Add gamer version in start menus

--- src/game/version.h
+++ src/game/version.h
@@ -3,7 +3,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include <generated/nethash.cpp>
-#define GAME_VERSION "0.7.3 - Gamer 5.4"
+#define GAME_VERSION "0.7.3.1 - Gamer 5.4"
 #define GAME_NETVERSION_HASH_FORCED "802f1be60a05665f"
 #define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH_FORCED
 #define CLIENT_VERSION 0x0703
